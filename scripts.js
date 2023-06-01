dfd9e59c54806cb2daf9058fef3a464470a75bb8
akijewski <adrnovsky1@gmail.com>
2023-06-01 15:12:18 +0200

    pause video after modal close

--- scripts.js
+++ scripts.js
@@ -2,13 +2,17 @@ var modal = document.getElementById("myModal");
 
 var span = document.getElementsByClassName("close")[0];
 
+var modalVideo = document.getElementById("modal-video");
+
 span.onclick = function () {
   modal.style.display = "none";
+  modalVideo.pause();
 };
 
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    modalVideo.pause();
   }
 };
 
